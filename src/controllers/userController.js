3b9488950a961f729f7a40b4781ad044956a4e7d
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-08-23 21:15:26 -0500

    Adding some comments

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,15 +2,29 @@ import { userService } from '../services/userService.js';
 
 export const userController = {
   getAll: async (req, res) => {
+    // Llamamos al servicio para encontrar todos los usuarios
     const allUsers = await userService.getAllUsers();
+
+    // Si no hay usuarios, le mandaremos un mensaje que no se encontró la información
+    !allUsers ??
+      res.status(404).json({ status: 404, message: 'Usuario no encontrado' });
+
     res
       .status(200)
       .json({ status: 200, total: allUsers.length, data: allUsers });
   },
 
   getOne: async (req, res) => {
+    // Capturamos el valor del id de la ruta
     const { id } = req.params;
+
+    // Llamamos al servicio para encontrar el usuario
     const user = await userService.getOneUser(id);
+
+    // Si el usuario no se encuentra en la base de datos, le mandaremos un mensaje que no se encontró
+    !user ??
+      res.status(404).json({ status: 404, message: 'Usuario no encontrado' });
+
     res.status(200).json({ status: 200, data: user });
   },
 
@@ -21,10 +35,13 @@ export const userController = {
   },
 
   delete: async (req, res) => {
+    // Capturamos el valor del id de la ruta
     const { id } = req.params;
 
+    // Llamamos al servicio para encontrar el usuario
     const findUser = await userService.getOneUser(id);
 
+    // Si el usuario no se encuentra en la base de datos, le mandaremos un mensaje que no se encontró
     !findUser ??
       res.status(404).json({ status: 404, message: 'Usuario no encontrado' });
 
@@ -33,13 +50,17 @@ export const userController = {
   },
 
   update: async (req, res) => {
+    // Capturamos el valor del id de la ruta
     const { id } = req.params;
 
+    // Llamamos al servicio para encontrar el usuario
     const oldDataUser = await userService.getOneUser(id);
 
+    // Si el usuario no se encuentra en la base de datos, le mandaremos un mensaje que no se encontró
     !oldDataUser ??
       res.status(404).json({ status: 404, message: 'Usuario no encontrado' });
 
+    // En caso que la información de lo que se cambie sea nueva, pisará la información vieja
     const userData = { ...oldDataUser, ...req.body };
     const userUpdated = await userService.updateUser(id, userData);
     res.status(200).json({ status: 200, isUpdated: true, data: userUpdated });

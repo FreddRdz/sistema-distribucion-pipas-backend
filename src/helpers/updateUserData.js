08e67ea1ba324fccfa5095784c8a7af8784833ad
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-09-05 17:08:31 -0500

    Applying performance in code

--- src/helpers/updateUserData.js
+++ src/helpers/updateUserData.js
@@ -1,8 +1,1 @@
-export const updateInfo = (newData, oldData) => {
-  return {
-    firstName: newData.firstName ?? oldData.firstName,
-    lastName: newData.lastName ?? oldData.lastName,
-    degree: newData.degree ?? oldData.degree,
-    password: newData.newPassword ?? oldData.password,
-  };
-};
+export const updateInfo = (newData, oldData) => newData ?? oldData;

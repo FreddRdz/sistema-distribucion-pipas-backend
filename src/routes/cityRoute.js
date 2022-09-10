bb9e6658163197e0497a2f5e951ba93282fc2f03
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-09-10 18:18:18 -0500

    Adding validations in many routes in project

--- src/routes/cityRoute.js
+++ src/routes/cityRoute.js
@@ -1,25 +1,68 @@
 import { Router } from 'express';
 import { cityController } from '../controllers/cityController.js';
+import { checkAuth } from '../middlewares/auth.js';
+import { checkRoleAuth } from '../middlewares/roleAuth.js';
+import {
+  validateCreateCity,
+  validateUpdateCity,
+} from '../validations/validationsCity.js';
 
 export const cityRouter = Router();
 
 // GET - http://localhost:5050/api/v1/cities
-cityRouter.get('/', cityController.getAll);
+cityRouter.get(
+  '/',
+  checkAuth,
+  checkRoleAuth(['admin, user']),
+  cityController.getAll
+);
 
 // GET - http://localhost:5050/api/v1/cities/:id
-cityRouter.get('/:id', cityController.getOne);
+cityRouter.get(
+  '/:id',
+  checkAuth,
+  checkRoleAuth(['admin, user']),
+  cityController.getOne
+);
 
 // POST - http://localhost:5050/api/v1/cities
-cityRouter.post('/', cityController.createOne);
+cityRouter.post(
+  '/',
+  checkAuth,
+  checkRoleAuth(['admin']),
+  validateCreateCity,
+  cityController.createOne
+);
 
 // DELETE - http://localhost:5050/api/v1/cities/:id
-cityRouter.delete('/:id', cityController.delete);
+cityRouter.delete(
+  '/:id',
+  checkAuth,
+  checkRoleAuth(['admin']),
+  cityController.delete
+);
 
 // UPDATE - http://localhost:5050/api/v1/cities/:id
-cityRouter.put('/:id', cityController.update);
+cityRouter.put(
+  '/:id',
+  checkAuth,
+  checkRoleAuth(['admin']),
+  validateUpdateCity,
+  cityController.update
+);
 
 // UPDATE PIPE - http://localhost:5050/api/v1/cities/add/:id/:pipe
-cityRouter.put('/add/pipe/:id/:pipe', cityController.addPipe);
+cityRouter.put(
+  '/add/pipe/:id/:pipe',
+  checkAuth,
+  checkRoleAuth(['admin']),
+  cityController.addPipe
+);
 
 // DELETE PIPE - http://localhost:5050/api/v1/cities/delete/:id/:pipe
-cityRouter.delete('/delete/pipe/:id/:pipe', cityController.deletePipe);
+cityRouter.delete(
+  '/delete/pipe/:id/:pipe',
+  checkAuth,
+  checkRoleAuth(['admin']),
+  cityController.deletePipe
+);
--- src/routes/pipeRoute.js
+++ src/routes/pipeRoute.js
@@ -1,19 +1,52 @@
 import { Router } from 'express';
 import { pipeController } from '../controllers/pipeController.js';
+import { checkAuth } from '../middlewares/auth.js';
+import { checkRoleAuth } from '../middlewares/roleAuth.js';
+import {
+  validateCreatePipe,
+  validateUpdatePipe,
+} from '../validations/validationsPipe.js';
 
 export const pipeRouter = Router();
 
 // GET - http://localhost:5050/api/v1/pipes
-pipeRouter.get('/', pipeController.getAll);
+pipeRouter.get(
+  '/',
+  checkAuth,
+  checkRoleAuth(['admin, user']),
+  pipeController.getAll
+);
 
 // GET - http://localhost:5050/api/v1/pipes/:id
-pipeRouter.get('/:id', pipeController.getOne);
+pipeRouter.get(
+  '/:id',
+  checkAuth,
+  checkRoleAuth(['admin, user']),
+  pipeController.getOne
+);
 
 // POST - http://localhost:5050/api/v1/pipes
-pipeRouter.post('/', pipeController.createOne);
+pipeRouter.post(
+  '/',
+  checkAuth,
+  checkRoleAuth(['admin']),
+  validateCreatePipe,
+  pipeController.createOne
+);
 
 // DELETE - http://localhost:5050/api/v1/pipes/:id
-pipeRouter.delete('/:id', pipeController.delete);
+pipeRouter.delete(
+  '/:id',
+  checkAuth,
+  checkRoleAuth(['admin']),
+  pipeController.delete
+);
 
 // UPDATE - http://localhost:5050/api/v1/pipes/:id
-pipeRouter.put('/:id', pipeController.update);
+pipeRouter.put(
+  '/:id',
+  checkAuth,
+  checkRoleAuth(['admin']),
+  validateUpdatePipe,
+  pipeController.update
+);
